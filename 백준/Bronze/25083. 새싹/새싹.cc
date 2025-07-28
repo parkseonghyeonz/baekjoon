5bc0aa5a5ad3e64bfc782603dbc6ab27bb669290
박성현 <sunghyeon5307@gmail.com>
2025-07-28 14:11:39 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    printf("         ,r'\"7\n");
+    printf("%c`-_   ,\'  ,/\n",'r');
+    printf(" \\. \". %c_%c'\n",'L','r');
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |");
+    return 0;
+}
